173d4c380a7ddfd447af01f98c1e7bd12a0d335c
E2-07 <E2-07@DENSHI-N08>
2023-10-30 16:24:37 +0900

    [新規追加]SETUP.hの追加を行いました (2023/10/30) セットアップ用ヘッダファイル、Project_ATCS_SETUP.hを作成しました。 なお、このファイルはPIC12F1822専用になります。 それ以外での機体の動作保証はできません。

---
+++ Project_ATCS_SETUP.h
@@ -0,0 +1,25 @@
+#ifndef SETUP_H
+#define	SETUP_H
+
+#include<xc.h>
+
+#pragma config FOSC = INTOSC    // Oscillator Selection 
+#pragma config WDTE = OFF       // Watchdog Timer Enable 
+#pragma config PWRTE = ON       // Power-up Timer Enable 
+#pragma config MCLRE = OFF      // MCLR Pin Function Select 
+#pragma config CP = OFF         // Flash Program Memory Code Protection 
+#pragma config CPD = OFF        // Data Memory Code Protection 
+#pragma config BOREN = ON       // Brown-out Reset Enable 
+#pragma config CLKOUTEN = OFF   // Clock Out Enable 
+#pragma config IESO = OFF       // Internal/External Switchover 
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable 
+
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection 
+#pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable 
+#pragma config BORV = HI        // Brown-out Reset Voltage Selection  
+#pragma config LVP = OFF        // Low-Voltage Programming 
+
+#endif	
+
+
