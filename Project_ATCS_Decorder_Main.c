3d7e9b1c0df982bf1d2182858263199572c1f34c
和匠 <139935269+WatakumiP@users.noreply.github.com>
2023-10-30 10:35:42 +0900

    [新規追加]Decorder_Mainのコメント (2023/10/30) Main一行目にあるコメント欄を修正しました

--- Project_ATCS_Decorder_Main.c
+++ Project_ATCS_Decorder_Main.c
@@ -1,12 +1,14 @@
 /*
- * File:   Project_ATCS.c
- * Author: owata
- *
+ * Project Auto Train Control System 
+ * Ver 1.0
+ * 
+ * Project Maid by WATANABE
  * Created on 2023/10/27, 12:07
  */
 
 
 #include <xc.h>
+
 
 void main(void) {
     return;
