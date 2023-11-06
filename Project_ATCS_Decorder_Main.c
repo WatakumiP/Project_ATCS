3e0935ff1fbfb77c7ef8bfc7ba8ce432185ae166
E2-07 <E2-07@DENSHI-N08>
2023-11-06 12:28:08 +0900

    [新規追加]RA2 立ち上がり割り込み制御用のプログラム追加
    RA2の立ち上がりを検出して、割り込みをかける部分の新規追加を行いました
    完成していないので、以降の製作にて作成していきます。

--- Project_ATCS_Decorder_Main.c
+++ Project_ATCS_Decorder_Main.c
@@ -9,10 +9,30 @@
 
 #include <xc.h>
 #define _XTAL_FREQ 8000000
-
+#include "Project_ATCS_SETUP.h"
 
 
 void main(void) {
     
+    OSCCON = 0x72;
+    ANSELA  = 0x00;
+    
+    TRISA = 0x02;
+    PORTA = 0x00;
+    
+    INTCON = 0xFC;
+    OPTION_REG = 0xC0;
+    
+    while(1){
+        
+    }    
     return;
+}
+unsigned int 
+void __interrupt() isr(void){
+    if(INTCONbits.INTF & ){
+        INTCONbits.INTF = 0;
+        
+    }
+
 }
